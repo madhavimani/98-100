c3557101585f5f5fe9e9d709a99aedb642b4d68f
madhavimani <152162952+madhavimani@users.noreply.github.com>
2024-04-19 19:28:25 +1000

    Add files via upload

---
+++ code.js
@@ -0,0 +1,38 @@
+function speak(){
+
+function setTimeout(){
+var imge_id("selfie")
+function take_snapshot(){
+    var speak_data = "taking your selfie in 5 seconds";
+    var utterthis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    setTimeout(1000 millisecond = 1second).
+}
+}
+function setTimeout(speak_data){
+    var imge_id = selfie1
+var speak_data
+
+}
+function setTimeout(speak_data){
+    var imge_id = selfie2
+var speak_data
+
+}
+}
+function take_snapshot(){
+    console.log(img_id)
+
+    Webcam.snap(function (data_uri)){
+        if(img_id=="selfie1"){
+        document.getElementById("result1").innerHTML = '<img id="selfie1" src="'+data uri+'"/>';
+    }
+}
+if(img_id=="selfie2"){
+    document.getElementById("result2").innerHTML = '<img id="selfie2" src="'+data uri+'"/>';
+}
+if(img_id=="selfie3"){
+    document.getElementById("result3").innerHTML = '<img id="selfie3if(img_id=="selfie1"){
+        
+}
+}
